9352690c67b8c0442b50de8bfb7165a794d47543
seoe5580 <seoe5580@wrdsb.ca>
2020-10-08 03:25:38 +0000

    Finished number counting question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program counts up the number from one all the way to user's number
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input a integer
+    System.out.println("Please enter a positive integer");
+
+    // Gets the number from user
+    int num = input.nextInt();
+
+    // starts at 1 and prints up to 1 number before num vaule and adds a ", " after every loop
+    for(int i = 1; i < num; i++){
+      System.out.print(i + ", ");
+    }
+
+    // prints out the last number out of the loop so it wouldnt loop 1, num 2, num 3, num
+    System.out.print(num);
     
   }
 }
